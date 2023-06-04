ad64c4b082e2541a228d4a82a9a31c9e7190b7d7
Zein Tonconi <zeintonconi@gmail.com>
2023-06-03 22:46:05 -0400

    Base

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
 
 
-const perfumes = [
-    {marca: "Christian Dior"},
-    {marca: "BVLGARI"},
-    {marca: "BURBERRY"},
+const perfumesMarcas = [
+    {marca: "Christian Dior", imgUrl: "img/diorPerfume.webp"},
+    {marca: "BVLGARI", imgUrl: "img/bvlgariPerfume.avif"},
+    {marca: "BURBERRY", imgUrl: "img/burberryPerfume.webp"},
     {marca: "Todos"}
 ]
 
-const navbarItems = [
-    {item: "Perfumes", subNavbar: perfumes},
-    {item: "Collares" },
-    {item: "Anillos" },
-    {item: "Aretes" },
-    {item: "Vapers"}
+const productos = [
+    {
+        nombre: "Chanel No 5 EAU premiere",
+        precio: 100,
+        descripcion: "tiene un aroma muy complejo de aldehídos y flores como rosas, jazmín, y lang-ylang, iris y lirio de los valles; todo esto sobre una base cálida y amaderada de sándalo, vainilla, ámbar y pachulí.",
+        tags: ["perfume", "Chanel"],
+        imgUrl: "img/chanel_No_5.png"
+    },
+    {
+        nombre: "Chanel Coco Mademoiselle",
+        precio: 60,
+        descripcion: "",
+        tags: ["perfume","Chanel"],
+        imgUrl: "img/chanelCoco.png"
+    }
+]
+
+const categories = [
+    {item: "Perfumes", subNavbar: perfumesMarcas, imgUrl: "img/categoriaPerfume.webp" },
+    {item: "Collares" , imgUrl: "img/categoriaCollar.jpg"},
+    {item: "Anillos", imgUrl: "img/categoriaAnillo.jpg" },
+    {item: "Aretes", imgUrl: "img/categoriaArete.jpeg"},
+    {item: "Vapers", imgUrl: "img/categoriaVaper.webp"}
 ]
 
 const cauroselItems = [
     {
         header: "",
         title: "Nueva Miss Dior Eau de Parfum",
-        imgUrl: "img/carousel-1.jpg" 
+        imgUrl: "img/gucciCampania.jpg" 
     },
     {
         header: "NUUBEZ Vaper Sin Nicotina",
@@ -30,7 +47,7 @@ const cauroselItems = [
 
 const navbar = () => {
     const navbar=document.getElementById('navbar-tienda-div');
-    navbarItems.forEach(item => {
+    categories.forEach(item => {
         
         if(item.subNavbar){
             const subNavbarDiv = document.createElement('div'); 
@@ -81,6 +98,51 @@ const carousel = () => {
 
 }
 
+const showTags = () => {
+    const categoryCards = document.getElementById('categoryCards');
+    categories.forEach(item => {
+        const div = document.createElement('div');
+        div.setAttribute('class', "col-lg-4 col-md-6 pb-1");
+        div.innerHTML = `
+                        <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px;">
+                            <a href="" class="cat-img position-relative overflow-hidden mb-3">
+                                <img class="img-fluid" src="${item.imgUrl}" alt="">
+                            </a>
+                            <h5 class="font-weight-semi-bold m-0">${item.item}</h5>
+                        </div>
+        `;
+        categoryCards.appendChild(div);
+    });
+}
+
+const showProductos = () => {
+    const perfumesCardsDiv = document.getElementById('perfumesCardsDiv');
+    productos.forEach(producto => {
+
+        const perfumeCard = document.createElement('div');
+        perfumeCard.setAttribute('class',"col-lg-3 col-md-6 col-sm-12 pb-1");
+        perfumeCard.innerHTML = `
+                                <div class="card product-item border-0 mb-4">
+                                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                        <img style="height:30rem;"class="img-fluid w-100" src="${producto.imgUrl}">
+                                    </div>
+                                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                        <h6 class="text-truncate mb-3">${producto.nombre}</h6>
+                                        <div class="d-flex justify-content-center">
+                                            <h6>$${producto.precio}</h6>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer d-flex justify-content-between bg-light border">
+                                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                                    </div>
+                                </div>`
+        perfumesCardsDiv.appendChild(perfumeCard)
+    })
+}
+
 
 navbar();
 carousel();
+showTags();
+showProductos();
